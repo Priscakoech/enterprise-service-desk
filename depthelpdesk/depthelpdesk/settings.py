6e4411c5595bddd28dc4f8ff2a0473e6649d18e1
Priscakoech <priscakoech448@gmail.com>
2026-02-16 12:11:39 +0300

    update environment variables

--- depthelpdesk/depthelpdesk/settings.py
+++ depthelpdesk/depthelpdesk/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-em&il9&k20d#b6si2091nzt$4#zcp4c$3y7t@-2913mzh$wu9v'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'tickets',
+    'departments',
+    'django',
 ]
 
 MIDDLEWARE = [
